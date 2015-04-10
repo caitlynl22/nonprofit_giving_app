e7d07d534484d0086df1d2568258669da5b3854d
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-04-10 11:28:11 -0400

    Remove Welcome to ember from application template

--- app/models/organization-profile.js
+++ app/models/organization-profile.js
@@ -2,5 +2,11 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  mission: DS.attr('string')
+  mission: DS.attr('string'),
+  description: DS.attr('string'),
+  image_url: DS.attr('string'),
+  website: DS.attr('string'),
+  address: DS.attr('string'),
+  contact: DS.attr('string'),
+  ein: DS.attr('string')
 });
